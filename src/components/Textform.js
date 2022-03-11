fb10b5d7850e87f9554ae7f8636fb972f7950624
udit <raiudit80@gmail.com>
2022-03-11 03:17:33 -0800

    few bugs removed

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -18,15 +18,7 @@ export default function Textform(props) {
     const handelOnChange=(event)=>{
         setText(event.target.value);
     }
-    const wordCount=(text)=>{
-        let arr=text.split(" ");
-        if(arr[arr.length-1]===""){
-            return arr.length-1;
-        }
-        else{
-            return arr.length;
-        }
-    }
+    
   return (
     <>
     <div className='conatainer' style={{color:props.mode==='dark'?'white':'black'}}>
@@ -34,13 +26,13 @@ export default function Textform(props) {
         <div className="mb-3">
         <textarea className="form-control" id="myBox" style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} rows="8" value={text} onChange={handelOnChange} placeholder="Enter your Text"></textarea>
         </div>
-        <button type="button" className="btn btn-primary mx-2" onClick={handelUpClick}>Convert to Uppercase</button>
-        <button type="button" className="btn btn-primary mx-2" onClick={handelloClick}>Convert to Lowercase</button>
-        <button type="reset" className="btn btn-primary mx-2" onClick={reset}>Reset</button>
+        <button type="button" className="btn btn-primary mx-2 my-4" onClick={handelUpClick}>Convert to Uppercase</button>
+        <button type="button" className="btn btn-primary mx-2 my-4" onClick={handelloClick}>Convert to Lowercase</button>
+        <button type="reset" className="btn btn-primary mx-2 my-4" onClick={reset}>Reset</button>
     </div>
     <div className="container my-4"  style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Text Summary</h2>
-        <p><b>Words:- </b>{wordCount(text)}</p>
+        <p><b>Words:- </b>{(text.split(" ").filter((a)=>{return a.length!==0;})).length}</p>
         <p><b>Characters:- </b>{text.length}</p> 
         <h2>Preview</h2>
         <p className='my-2'>{text.length>0 ?text:'Enter Some Text In TextBox To Preview'}</p>
